076b57b800b9362acaa3d1e050693bdf40aafbf3
SSnoopy <shahaan.salman1@gmail.com>
2024-03-06 00:12:23 +0800

    Basic Touchup
    - Fixed some of the trees overlapping in the intial scene as well as some other small map edits.
    - Attached camera to the player and added lerp for smoothing
    - Animation still broken.(Working on it)

--- GameDevProject/Characters/character_body_2d.gd
+++ GameDevProject/Characters/character_body_2d.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0,1)
+
+@onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
+
+
+func _ready():
+	update_animation_parameters(starting_direction)
+
 
 func _physics_process(_delta):
 	var direction = Vector2(
@@ -9,5 +18,17 @@ func _physics_process(_delta):
 	)
 	
 	velocity = direction * move_speed 
-
 	move_and_slide()
+
+
+
+func update_animation_parameters(move_input : Vector2):
+	if(move_input != Vector2.ZERO):
+		animation_tree.set("parameters/walk/blend_position", move_input)
+		animation_tree.set("parameters/idle/blend_position", move_input)
+
+func pick_new_state():
+	if(velocity != Vector2.ZERO):
+		state_machine.travel("Walking")		
+	else:
+		state_machine.travel("Idle")
